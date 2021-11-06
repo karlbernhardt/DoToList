6696755326eb1caf2177361c228d10f875f0b4a7
Karl Bernhardt <karlbernhardt50@gmail.com>
2021-11-06 10:57:51 +0200

    Initial Commit

---
+++ DoToList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoToList
+//
+//  Created by Karl Bernhardt on 11/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
