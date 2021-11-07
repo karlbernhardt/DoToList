801fd927d872da3499387d6667c73c2cadceffcf
Karl Bernhardt <karlbernhardt50@gmail.com>
2021-11-07 12:40:22 +0200

    Delete, move cells. Segue's and transfer of data

--- DoToList/ToDoViewController.swift
+++ DoToList/ToDoViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ToDoViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
     
     //Variables
     var toDoArray = ["Learn Swift","Build Apps","Change the World","Take a Vacation"]
@@ -21,6 +23,51 @@ class ToDoViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            let destination = segue.destination as! ToDoDetailTableViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.toDoItem = toDoArray[selectedIndexPath.row]
+        }else {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow{
+                tableView.deselectRow(at: selectedIndexPath, animated: true)
+            }
+        }
+    }
+    
+    @IBAction func unwindFromDetail(segue: UIStoryboardSegue){
+        let source = segue.source as! ToDoDetailTableViewController
+        if let selectedIndexPath = tableView.indexPathForSelectedRow{
+            toDoArray[selectedIndexPath.row] = source.toDoItem
+            tableView.reloadRows(at: [selectedIndexPath], with: .automatic)
+        }else {
+            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+            toDoArray.append(source.toDoItem)
+            tableView.insertRows(at: [newIndexPath], with: .bottom)
+            tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
+        }
+    }
+    
+    
+
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        
+        if tableView.isEditing {
+            tableView.setEditing(false, animated: true)
+            sender.title = "Edit"
+            addBarButton.isEnabled = true
+        }else{
+            tableView.setEditing(true, animated: true)
+            sender.title = "Done"
+            addBarButton.isEnabled = false
+        }
+        
+        
+    }
+    
+    
+    
 }
 
 extension ToDoViewController: UITableViewDelegate, UITableViewDataSource {
@@ -35,6 +82,19 @@ extension ToDoViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = toDoArray[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            toDoArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = toDoArray[sourceIndexPath.row]
+        toDoArray.remove(at: sourceIndexPath.row)
+        toDoArray.insert(itemToMove, at: destinationIndexPath.row)
     }
     
     
