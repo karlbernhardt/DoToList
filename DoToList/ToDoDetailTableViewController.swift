22f21dda176f4ecdb1c021ee5bec276924b7afdf
Karl Bernhardt <karlbernhardt50@gmail.com>
2021-11-08 21:00:39 +0200

    Structs, project wide variables

--- DoToList/ToDoDetailTableViewController.swift
+++ DoToList/ToDoDetailTableViewController.swift
@@ -16,20 +16,22 @@ class ToDoDetailTableViewController: UITableViewController {
     
     //Variables
     
-    var toDoItem: String!
+    var toDoItem: ToDoItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if toDoItem == nil{
-            toDoItem = ""
+            toDoItem = ToDoItem(name: "", date: Date(), notes: "")
         }
-        nameField.text = toDoItem
-
+        nameField.text = toDoItem.name
+        datePicker.date = toDoItem.date
+        noteView.text = toDoItem.notes
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        toDoItem = nameField.text
+        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text)
     }
 
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
---
+++ DoToList/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  DoToList
+//
+//  Created by Karl Bernhardt on 11/8/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
