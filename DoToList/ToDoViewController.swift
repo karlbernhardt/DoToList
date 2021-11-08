a9a3797ebed832dff87825b8ed422f275a74e709
Karl Bernhardt <karlbernhardt50@gmail.com>
2021-11-08 21:25:49 +0200

    Savings Data to phone storage, Loading Data from Storage of iphone

--- DoToList/ToDoViewController.swift
+++ DoToList/ToDoViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 
 
-
-
-
-
 class ToDoViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
@@ -27,6 +23,34 @@ class ToDoViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        loadData()
+    }
+    
+    func loadData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+        
+        guard  let data = try? Data(contentsOf: documentURL) else {return}
+        let jsonDecoder = JSONDecoder()
+            do{
+                toDoItems = try jsonDecoder.decode(Array<ToDoItem>.self, from: data)
+                tableView.reloadData()
+            }catch{
+                print("Error: Could not load data")
+            }
+    }
+    
+    func saveData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+        let jsonEncoder = JSONEncoder()
+        let data = try? jsonEncoder.encode(toDoItems)
+        do{
+            try data?.write(to: documentURL, options: .noFileProtection)
+        }catch{
+            print("Error: Could not save data")
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -52,8 +76,8 @@ class ToDoViewController: UIViewController {
             tableView.insertRows(at: [newIndexPath], with: .bottom)
             tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
         }
+        saveData()
     }
-    
     
 
     @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
@@ -67,12 +91,7 @@ class ToDoViewController: UIViewController {
             sender.title = "Done"
             addBarButton.isEnabled = false
         }
-        
-        
     }
-    
-    
-    
 }
 
 extension ToDoViewController: UITableViewDelegate, UITableViewDataSource {
@@ -94,6 +113,7 @@ extension ToDoViewController: UITableViewDelegate, UITableViewDataSource {
         if editingStyle == .delete{
             toDoItems.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            saveData()
         }
     }
     
@@ -101,6 +121,7 @@ extension ToDoViewController: UITableViewDelegate, UITableViewDataSource {
         let itemToMove = toDoItems[sourceIndexPath.row]
         toDoItems.remove(at: sourceIndexPath.row)
         toDoItems.insert(itemToMove, at: destinationIndexPath.row)
+        saveData()
     }
     
     
